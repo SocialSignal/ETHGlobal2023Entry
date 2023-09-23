8387d00ebdeb18cd92e71f2d51636a1a38c0274a
lightwalker.eth <126201998+lightwalker-eth@users.noreply.github.com>
2023-09-23 11:39:43 -0400

    Define ValueReference type

--- website/types/types.ts
+++ website/types/types.ts
@@ -64,3 +64,41 @@ export type TribeSummary = {
     memberCount: number;
 
 };
+
+export type ValueReference = {
+
+    /*
+     * Used to decide if two values are equal with each other. Making it so that spaces or
+     * capitalization differences don’t result in two values being considered as different
+     * that are actually the same.
+     * 
+     * Rule to generate:
+     *      Given a raw input string containing any number of comma separated values:
+     *      Split using “,”
+     *      For each token returned:
+     *          Remove all whitespace characters
+     *          If the token is an empty string, discard the token
+     *          Convert to lowercase
+     */
+    normalizedValue: string;
+
+    /*
+     * Used at a UI level to display a value in a more readable format.
+     * NOTE: two values that are considered to be “shared” (equal with each other)
+     * will always have the same Normalized Value, but may have different Display Values.
+     * 
+     * Rule to generate:
+     *      Given a raw input string containing any number of comma separated values:
+     *      Split using “,”
+     *      For each token returned:
+     *          Remove any leading or trailing whitespace characters
+     *          If the token is an empty string, discard the token
+     */
+    displayValue: string;
+
+    /*
+     * Identifies if the value being used is also held by the “viewed-from” user account.
+     * null if and only if the provided “viewed-from” param is NOT defined in the API request.
+     */
+    sharedWithViewer: boolean | null;
+};
