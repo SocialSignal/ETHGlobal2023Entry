f21be3fe240b8c060b49087162742cf5ba9e7369
lightwalker.eth <126201998+lightwalker-eth@users.noreply.github.com>
2023-09-23 16:35:13 -0400

    Adding AccountSummary objects to TribeDetail Mock API

--- website/lib/api/utils.ts
+++ website/lib/api/utils.ts
@@ -1,4 +1,18 @@
-import { ENSProfile, TribeReference, TribeSummary, ValueReference } from "../../types/types";
+import { AccountSummary, ENSProfile, LensProfile, SocialStorySource, SocialStorySummary, TribeReference, TribeSummary, ValueReference, XMTPProfile } from "../../types/types";
+
+export function buildMockENSProfile(address: string, values: ValueReference[]): ENSProfile {
+    return {
+        values: values,
+        displayName: "Unknown " + address.substring(0, 4),
+        avatar: `/address/${address}/avatar`,
+        description: "We help companies by developing tailor-made blockchain solutions using top-notch technology.",
+        twitter: "Blockful_io",
+        email: "info@blockful.io",
+        website: "https://blockful.io",
+        github: "blockful-io",
+        telegram: "blockful_io"
+      };
+}
 
 export function buildMockTribeSummary(chainId: number, tribeAddress: string, displayName: string, owner: string, memberCount: number, values: ValueReference[]): TribeSummary {
     const tribeId : TribeReference = {
@@ -6,17 +20,7 @@ export function buildMockTribeSummary(chainId: number, tribeAddress: string, dis
       address: tribeAddress
     };
   
-    const ensProfile : ENSProfile = {
-      values: values,
-      displayName: "Unknown " + tribeAddress.substring(0, 4),
-      avatar: `/address/${tribeAddress}/avatar`,
-      description: "We help companies by developing tailor-made blockchain solutions using top-notch technology.",
-      twitter: "Blockful_io",
-      email: "info@blockful.io",
-      website: "https://blockful.io",
-      github: "blockful-io",
-      telegram: "blockful_io"
-    };
+    const ensProfile = buildMockENSProfile(tribeAddress, values);
   
     return {
       tribeId: tribeId,
@@ -28,3 +32,37 @@ export function buildMockTribeSummary(chainId: number, tribeAddress: string, dis
       ensProfile: ensProfile
     };
   }
+
+export function buildMockSocialStorySummary(source: SocialStorySource, description: string, sharedWithViewer: boolean): SocialStorySummary {
+    const badge = `/social-story/${source}/badge`;
+    return {
+        source: source,
+        description: description,
+        badge: badge,
+        sharedWithViewer: sharedWithViewer
+    };
+}
+
+export function buildMockAccountSummary(address: string, primaryTribe: TribeSummary | null, curatedSocialStories: SocialStorySummary[], values: ValueReference[]) : AccountSummary {
+
+    const socialStory1 = buildMockSocialStorySummary(SocialStorySource.Tribe, "Also a member of Tribe 1", true);
+    const socialStory2 = buildMockSocialStorySummary(SocialStorySource.Value, "Also values Environment", true);
+    const socialStory3 = buildMockSocialStorySummary(SocialStorySource.Poap, "Also attended Devcon Bogota", true);
+    const socialStory4 = buildMockSocialStorySummary(SocialStorySource.Nft, "Owns a CryptoPunk", false)
+    const socialStory5 = buildMockSocialStorySummary(SocialStorySource.Value, "Values Freedom", false);
+
+    return {
+        address: address,
+        metaProfile: {
+            ensProfile: buildMockENSProfile(address, values),
+            lensProfile: {
+                lensName: "ExampleLensName",
+            },
+            xmtpProfile: {
+                activatedXMTP: true,
+            }
+        },
+        curatedSocialStories: [socialStory1, socialStory2, socialStory3, socialStory4, socialStory5], // TODO: Ensure not more than MAX_SOCIAL_STORY_SUMMARIES
+        primaryTribe: primaryTribe
+    };
+}
--- website/pages/api/tribes/[chainId]/[tribeAddress]/index.ts
+++ website/pages/api/tribes/[chainId]/[tribeAddress]/index.ts
@@ -1,7 +1,7 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import { TribeDetail, TribeReference } from "../../../../../types/types";
 import { buildValueReferences } from "../../../../../lib/shared/utils";
-import { buildMockTribeSummary } from "../../../../../lib/api/utils";
+import { buildMockAccountSummary, buildMockTribeSummary } from "../../../../../lib/api/utils";
 
 // TODO: validation with zod
 type RequestData = {
@@ -60,13 +60,18 @@ export default function getTribeDetail(
   const values = buildValueReferences("Web3, Decentralization, LOVE, Environment", null);
   const tribeSummary = buildMockTribeSummary(1, "0x283af0b28c62c092c9727f1ee09c02ca627eb7f5", "Tribe 1", "0xd8da6bf26964af9d7eed9e03e53415d37aa96045", 123, values);
 
-  // TODO: build AccountSummary objects !
+  const account1 = buildMockAccountSummary("0x1a199654959140e5c1a2f4135faa7ba2748939c5", tribeSummary, values);
+  const account2 = buildMockAccountSummary("0x76a6d08b82034b397e7e09dae4377c18f132bbb8", null, values);
+  const account3 = buildMockAccountSummary("0xfe89cc7abb2c4183683ab71653c4cdc9b02d44b7", null, values);
+  const account4 = buildMockAccountSummary("0x0364c42a15c2cc3073eba1e11ee5ab0c6a1b5b40", null, values);
+  const account5 = buildMockAccountSummary("0xa9350e3b4ad3f22bab136cfef999c132ead3bca3", null, values);
+  const account6 = buildMockAccountSummary("0xfa45c6991a2c3d74ada3a279e21135133ce3da8a", null, values);
 
   const tribe: TribeDetail = {
     ...tribeSummary,
-    members: [],
-    supporters: [],
-    invites: [],
+    members: [account1, account2],
+    supporters: [account3, account4],
+    invites: [account5, account6],
   };
 
   const response: ResponseData = {
