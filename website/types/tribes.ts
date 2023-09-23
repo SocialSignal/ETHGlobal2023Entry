7c8af2961d10a20c73b3989f734d970b2f145e31
lightwalker.eth <126201998+lightwalker-eth@users.noreply.github.com>
2023-09-23 11:27:48 -0400

    Define TribeSummary type

--- website/types/tribes.ts
+++ website/types/tribes.ts
@@ -2,15 +2,65 @@ export type TribeReference = {
     
     /*
      * The chain ID where the Tribe was created.
+     * Source: Tribe Smart Contract.
      * Contains a non-negative integer value.
-     * Source: Tribe Smart Contract -> Subgraph -> Backend -> Frontend
-    */
+     */
     chainId: number;
 
     /*
      * The address of the Tribe Smart Contract on the related `chainId`.
+     * Source: Tribe Smart Contract.
      * Contains a lowercase hex string beginning with "0x".
-     * Source: Tribe Smart Contract -> Subgraph -> Backend -> Frontend
-    */
+     */
     address: string;
+};
+
+export type TribeSummary = {
+
+    /*
+     * Unique identifier for the Tribe.
+     */
+    tribeId: TribeReference;
+
+    /*
+     * The name of the Tribe.
+     * Source: Tribe Smart Contract.
+     * Contains a string of up to 255 characters (as truncated by backend).
+     */
+    displayName: string;
+
+    /*
+     * The address of the owner of the Tribe Smart Contract.
+     * Source: Tribe Smart Contract
+     * Contains a lowercase hex string beginning with "0x".
+     */
+    owner: string;
+
+    /*
+     * URL to large image of the Tribe in our own backend.
+     * Requests to this URL will ask our backend to do the work to ultimately
+     * resolve this image and return it as image data.
+     * 
+     * Url will always be in the format `/tribes/${chainId}/${tribeAddress}/largeImage`.
+     * 
+     */
+    largeImage: string;
+
+    /*
+     * URL to small badge image of the Tribe in our own backend.
+     * Requests to this URL will ask our backend to do the work to ultimately
+     * resolve this image and return it as image data.
+     * 
+     * Url will always be in the format `/tribes/${chainId}/${tribeAddress}/smallBadge`.
+     * 
+     */
+    smallBadge: string;
+
+    /*
+     * The number of members in the Tribe.
+     * Always a non-negative integer.
+     * Source: Tribe Smart Contract
+     */
+    memberCount: number;
+
 };
