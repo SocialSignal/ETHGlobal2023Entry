3e2c52e77d7e2162594188a69255c27426dcbbf8
lightwalker.eth <126201998+lightwalker-eth@users.noreply.github.com>
2023-09-24 06:35:03 -0400

    Add Airstack

--- website/lib/api/utils.ts
+++ website/lib/api/utils.ts
@@ -2,6 +2,7 @@ import { AccountSummary, ENSProfile, LensProfile, SocialStorySource, SocialStory
 import { ethers } from 'ethers'
 import { buildValueReferences } from "../shared/utils";
 import { ens_normalize } from '@adraffy/ens-normalize';
+import { init as initAirstack, fetchQuery } from "@airstack/node";
 
 export type Providers = {
     chainId: number,
@@ -115,7 +116,9 @@ export async function buildMockTribeSummary(providers: Providers, tribeAddress: 
     };
   }
 
-export function buildMockSocialStorySummary(source: SocialStorySource, description: string, sharedWithViewer: boolean): SocialStorySummary {
+
+  
+export function buildSocialStorySummary(source: SocialStorySource, description: string, sharedWithViewer: boolean): SocialStorySummary {
     const badge = `/social-story/${source}/badge`;
     return {
         source: source,
@@ -125,13 +128,183 @@ export function buildMockSocialStorySummary(source: SocialStorySource, descripti
     };
 }
 
+
+// let poaps = [];
+
+// data.Poaps.Poap.forEach((poap) => {
+//     poap.poapEvent.poaps.forEach((poapEvent) => {
+//         poapEvent.
+//     });
+// }
+
+/*
+
+{
+  "data": {
+    "Poaps": {
+      "Poap": [
+        {
+          "poapEvent": {
+            "poaps": [
+              {
+                "poapEvent": {
+                  "eventName": "You have met Patricio in September of 2023 (IRL)"
+                }
+              }
+            ]
+          }
+        },
+        {
+          "poapEvent": {
+            "poaps": []
+          }
+        },
+        {
+          "poapEvent": {
+            "poaps": [
+              {
+                "poapEvent": {
+                  "eventName": "You've met Lucas in Summer 23"
+                }
+              }
+            ]
+          }
+        },
+        {
+          "poapEvent": {
+            "poaps": [
+              {
+                "poapEvent": {
+                  "eventName": "You have met Patricio in April of 2023 (IRL)"
+                }
+              }
+            ]
+          }
+        },
+        {
+          "poapEvent": {
+            "poaps": []
+          }
+        },
+        {
+          "poapEvent": {
+            "poaps": [
+              {
+                "poapEvent": {
+                  "eventName": "Airstack Amazing Race Dubai"
+                }
+              }
+            ]
+          }
+        },
+        {
+          "poapEvent": {
+            "poaps": [
+              {
+                "poapEvent": {
+                  "eventName": "You have met Patricio in July of 2023 (IRL)"
+                }
+              }
+            ]
+          }
+        },
+        {
+          "poapEvent": {
+            "poaps": [
+              {
+                "poapEvent": {
+                  "eventName": "ETHGlobal Tokyo 2023 Partner Attendee"
+                }
+              }
+            ]
+          }
+        },
+        {
+          "poapEvent": {
+            "poaps": []
+          }
+        },
+        {
+          "poapEvent": {
+            "poaps": [
+              {
+                "poapEvent": {
+                  "eventName": "You have met Patricio in June of 2023 (IRL)"
+                }
+              }
+            ]
+          }
+        },
+        {
+          "poapEvent": {
+            "poaps": []
+          }
+        },
+        {
+          "poapEvent": {
+            "poaps": []
+          }
+        },
+        {
+          "poapEvent": {
+            "poaps": [
+              {
+                "poapEvent": {
+                  "eventName": "ETHGlobal Paris 2023 Partner Attendee"
+                }
+              }
+            ]
+          }
+        },
+        {
+          "poapEvent": {
+            "poaps": []
+          }
+        }
+      ]
+    }
+  }
+}
+
+
+ */
+
+
+
 export async function buildMockAccountSummary(providers: Providers, address: string, primaryTribe: TribeSummary | null, viewerValues: ValueReference[]) : Promise<AccountSummary> {
 
-    const socialStory1 = buildMockSocialStorySummary(SocialStorySource.Tribe, "Also a member of Tribe 1", true);
-    const socialStory2 = buildMockSocialStorySummary(SocialStorySource.Value, "Also values Environment", true);
-    const socialStory3 = buildMockSocialStorySummary(SocialStorySource.Poap, "Also attended Devcon Bogota", true);
-    const socialStory4 = buildMockSocialStorySummary(SocialStorySource.Nft, "Owns a CryptoPunk", false)
-    const socialStory5 = buildMockSocialStorySummary(SocialStorySource.Value, "Values Freedom", false);
+    const airstackKey = process.env.AIRSTACK_KEY;
+    if (!airstackKey) {
+        throw new Error("You need to provide AIRSTACK_KEY env variable");
+    }
+
+    initAirstack(airstackKey);
+
+    const viewerAddress = "0x283Af0B28c62C092C9727F1Ee09c02CA627EB7F5".toLowerCase();
+
+    const getCommonPOAPQuery = `query MyQuery {
+        Poaps(
+          input: {filter: {owner: {_eq: "${address}"}}, blockchain: ALL, limit: 10}
+        ) {
+          Poap {
+            poapEvent {
+              poaps(input: {filter: {owner: {_eq: "${viewerAddress}"}}}) {
+                poapEvent {
+                  eventName
+                }
+              }
+            }
+          }
+        }
+      }`;
+
+    const { data, error } = await fetchQuery(getCommonPOAPQuery);
+
+    const socialStory1 = buildSocialStorySummary(SocialStorySource.Tribe, "Also a member of Tribe 1", true);
+    const socialStory2 = buildSocialStorySummary(SocialStorySource.Value, "Also values Environment", true);
+    const socialStory3 = buildSocialStorySummary(SocialStorySource.Poap, "Also attended Devcon Bogota", true);
+    const socialStory4 = buildSocialStorySummary(SocialStorySource.Nft, "Owns a CryptoPunk", false)
+    const socialStory5 = buildSocialStorySummary(SocialStorySource.Value, "Values Freedom", false);
 
     let ensProfile = await buildRealENSProfile(providers, address, viewerValues);
 
